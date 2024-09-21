5536b6c6fe9a3096f5c3be558ef4b1e6e0e48c9d
yunboard <ayrc8980@gmail.com>
2024-09-21 14:41:23 +0800

    1.2.10

---
+++ experiment/web.py/iters.py
@@ -0,0 +1,10 @@
+# def my_generator(limit):
+#     counter = 0
+#     while counter < limit:
+#         yield counter
+#         counter += 1
+
+# # 使用生成器
+# gen = my_generator(5)
+# for num in gen:
+#     print(num)

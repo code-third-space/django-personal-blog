6d6839e9c51bc5a4bb494dd2f0f9a75f492e7fac
yunboard <ayrc8980@gmail.com>
2025-04-12 17:06:15 +0800

    1.3.25 --为创建 dev 分支前的最后一次提交

--- experiment/web.py/meeting_room/recruitment/meet_project/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery import Celery
-
-app = Celery('tasks', backend='redis://127.0.0.1', broker='redis://127.0.0.1')
-
-@app.task
-def add(x, y):
-    return x + y
--- experiment/web.py/meeting_room/recruitment/running/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RunningConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'running'

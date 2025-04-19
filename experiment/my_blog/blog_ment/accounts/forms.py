2c1e6f4102676dc9aa5e60c06a7f62f39753f2f9
yunboard <ayrc8980@gmail.com>
2025-04-19 11:12:51 +0800

    --个人资料页面的修改

--- experiment/my_blog/blog_ment/accounts/forms.py
+++ experiment/my_blog/blog_ment/accounts/forms.py
@@ -18,3 +18,23 @@ class CustomUserChangeForm(UserChangeForm):
         model = CustomUser
         fields = ('username', 'email', 'phone', 'city', 'gener', 'user_remark', 'picture', 'back_ground', 'user_type','social_links', 'blog_count' )
         exclude = ('password',)  # 排除密码字段，因为密码通常不应该在修改表单中显示
+
+
+class ProfileForm(forms.ModelForm):
+    """
+    用户个人资料编辑表单
+    """
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'user_remark', 'picture', 'phone')
+        labels = {
+            'user_remark': '个人简介',
+            'picture': '头像',
+        }
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'user_remark': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'picture': forms.FileInput(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
